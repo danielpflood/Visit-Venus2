c792f9a4d07360b9c410d87bb483866c0c7d5454
Daniel Flood <danielpflood@gmail.com>
2012-04-23 14:56:54 -0400

    added admin attr to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120423172715) do
+ActiveRecord::Schema.define(:version => 20120423185437) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120423172715) do
     t.string   "password_salt"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.boolean  "admin"
   end
 
   create_table "vevents", :force => true do |t|
